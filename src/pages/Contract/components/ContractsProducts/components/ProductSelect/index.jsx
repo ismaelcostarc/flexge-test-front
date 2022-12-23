7bcb36226d128bfe7fdaffffb669b63dbfb8dfdf
Ismael Costa <ismael.costa@clicksign.com>
2022-12-22 23:13:41 -0300

    feat: form to add new products

---
+++ src/pages/Contract/components/ContractsProducts/components/ProductSelect/index.jsx
@@ -0,0 +1,39 @@
+import { Select, Form } from "antd";
+import { AuthContext } from "../../../../../../contexts/auth";
+import { useContext, useEffect, useState } from "react";
+import { api } from "../../../../../../services/api";
+
+export const ProductSelect = () => {
+  const { token } = useContext(AuthContext);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const { data } = await api.get("/product", {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        });
+        const products = data.map((country, index) => ({
+          value: country.name,
+          label: country.name,
+          key: index,
+          id: country._id
+        }));
+
+        setOptions(products);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getProducts();
+  }, []);
+
+  return (
+    <Form.Item name="product" colon={false}>
+      <Select placeholder="Select..." options={options} />
+    </Form.Item>
+  );
+};
--- src/pages/Contract/components/ContractsProducts/index.jsx
+++ src/pages/Contract/components/ContractsProducts/index.jsx
@@ -5,21 +5,81 @@ import {
   Card,
   Table,
   Input,
-  Select,
   Space,
   Col,
   DatePicker,
   Typography,
 } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import { PlusOutlined, EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { ProductSelect } from "./components/ProductSelect";
+import { useState } from "react";
+import { useForm } from "antd/es/form/Form";
+import dayjs from "dayjs";
 
 const { Text } = Typography;
 
 export const ContractsProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [form] = useForm();
+
+  const addProduct = (values) => {
+    const newProduct = values.beginningOfTerm
+      ? {
+          ...values,
+          beginningOfTerm: new Date(
+            values.beginningOfTerm.format("YYYY-MM-DD")
+          ),
+          key: products.length,
+        }
+      : {
+          ...values,
+          key: products.length,
+        };
+
+    setProducts([...products, newProduct]);
+    form.resetFields();
+  };
+
+  const columns = [
+    { title: "Product", dataIndex: "product", key: "product" },
+    { title: "Amount", dataIndex: "amount", key: "amount" },
+    {
+      title: "Final Unit Price",
+      dataIndex: "finalUnitPrice",
+      key: "finalUnitPrice",
+    },
+    { title: "Installments", dataIndex: "installments", key: "installments" },
+    {
+      title: "Paid Installments",
+      dataIndex: "paidInstallments",
+      key: "paidInstallments",
+    },
+    {
+      title: "Beginning Of Term",
+      dataIndex: "beginningOfTerm",
+      key: "beginningOfTerm",
+    },
+    {
+      title: "Actions",
+      key: "finalUnitPrice",
+      width: "10%",
+      render: (_, record, index) => (
+        <Space size="middle">
+          <Button onClick={() => {}} type="text">
+            <EditOutlined />
+          </Button>
+          <Button onClick={() => {}} type="text">
+            <DeleteOutlined />
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
   return (
     <Card title="Contract's Products">
       <Space direction="vertical" style={{ width: "100%" }} size="middle">
-        <Form layout="inline">
+        <Form layout="inline" onFinish={addProduct} form={form}>
           <Space direction="vertical" style={{ width: "100%" }} size="small">
             <Row gutter={24} style={{ width: "100%" }}>
               <Col span={6}>
@@ -45,9 +105,7 @@ export const ContractsProducts = () => {
 
             <Row gutter={24} style={{ width: "100%" }}>
               <Col span={6}>
-                <Form.Item name="product">
-                  <Select></Select>
-                </Form.Item>
+                <ProductSelect />
               </Col>
               <Col span={3}>
                 <Form.Item name="amount">
@@ -71,12 +129,15 @@ export const ContractsProducts = () => {
               </Col>
               <Col span={3}>
                 <Form.Item name="beginningOfTerm">
-                  <DatePicker />
+                  <DatePicker
+                    initialValues={dayjs("01/01/2015", "DD/MM/YYYY")}
+                    format={["DD/MM/YYYY", "DD/MM/YY"]}
+                  />
                 </Form.Item>
               </Col>
               <Col span={3}>
                 <Form.Item>
-                  <Button block>
+                  <Button block htmlType="submit">
                     <PlusOutlined /> Add
                   </Button>
                 </Form.Item>
@@ -84,7 +145,7 @@ export const ContractsProducts = () => {
             </Row>
           </Space>
         </Form>
-        <Table></Table>
+        <Table columns={columns} dataSource={products}></Table>
       </Space>
     </Card>
   );
